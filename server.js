76735c8502cfbdeee255359565d6da8e0110cc9e
Sunny <sy17012002@gmail.com>
2026-03-24 18:40:53 +0530

    Updated

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(express.static("dashboards"));
 
 // Session setup
 app.use(session({

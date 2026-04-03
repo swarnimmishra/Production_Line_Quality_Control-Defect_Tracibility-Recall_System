657df74003dc465048448f1e8863f8de843facea
Swarnim Mishra <smswarnimmishra@gmail.com>
2026-04-03 19:48:50 +0530

    recall update

--- routes/recall.js
+++ routes/recall.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const path = require("path");
 const supabase = require("../supabaseClient");
@@ -20,7 +19,7 @@ router.post("/trace", async (req, res) => {
             .eq("batch_id", batch_id);
 
         if (error) throw error;
-        
+
         if (!data || data.length === 0) {
             return res.json({ shipments: [] });
         }
@@ -30,6 +29,51 @@ router.post("/trace", async (req, res) => {
         console.error(err);
         res.status(500).json({ message: "Server error" });
     }
+});
+
+// Recall defective shipment
+router.post("/recall_shipment", async (req, res) => {
+  try {
+    const { shipment_id } = req.body;
+
+    if (!shipment_id) {
+        return res.status(400).json({ message: "Shipment ID is required" });
+    }
+
+    const { data, error } = await supabase
+        .from("shipments")
+        .select("*")
+        .eq("shipment_id", shipment_id);
+
+    if (error) throw error;
+
+    if (!data || data.length === 0) {
+        return res.status(400).json({ message: "Shipment not found" });
+    }
+
+    const shipmentStatus = data[0].status;
+    const recallableStatuses = ["In Transit", "Shipped"];
+
+    if (recallableStatuses.includes(shipmentStatus)) {
+      const { data, error } = await supabase
+          .from("shipments")
+          .update({ status: "Recalled" })
+          .eq("shipment_id", shipment_id);
+
+      if (error) throw error;
+
+      res.json({ message: `Shipment ID ${shipment_id} Recalled` });
+
+    } else {
+      if (shipmentStatus === "Recalled")
+      res.json({ message: "Error: Shipment already recalled" });
+      else
+      res.json({ message: `Error: Shipment is ${shipmentStatus}` });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 module.exports = router;
